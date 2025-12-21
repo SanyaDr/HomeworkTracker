624f1ef915219451066e4da53811373203a22c19
Alexander <mr.drovosekov2006@yandex.ru>
2025-12-21 22:15:42 +0300

    fix auth by email

--- backend/app/crud/users.py
+++ backend/app/crud/users.py
@@ -53,19 +53,12 @@ def create_user(db: Session, user):
 def authenticate_user(db: Session, login: str, password: str) -> Optional["User"]:
     """Аутентификация пользователя"""
     user = get_user_by_login(db, login)
-    # # print("начал входить")
-    # if not user:
-    #     # print("Не нашел user по логину")
-    # if not user:
-    #     user = get_user_by_email(db, login)
-    #     if not user:
-    #         # print("Не нашел user по email")
     if not user:
-        return False
+        user = get_user_by_email(db, login)
+        if not user:
+            return False
     if not verify_password(password, user.hashed_password):
-        # print("пароль неверный")
         return False
-    # print("Вошел вернул user")
     return user
 
 
